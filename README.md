1463fd96562bd42266e6f18df444b0ea830baff1
garyyb <garyybai@gmail.com>
2019-04-18 19:22:39 +1000

    Manage dependencies with Closure.

--- README.md
+++ README.md
@@ -3,3 +3,13 @@
 
 ## Examples
 Typing `c/` will open Google Calendar.
+
+## Building from Source
+You need a valid closure compiler aliased to `closurecompiler`. Then, execute:
+
+```bash
+closurecompiler --js "src/**.js" --js_output_file build/background-compiled.js
+```
+
+Alternatively, if you don't want to alias the compiler, just replace `closurecompiler`
+with `java -jar /path/to/closurecompiler.jar`.
---
+++ src/util/messenger.js
@@ -0,0 +1,41 @@
+goog.module('shortlinks.util.messenger');
+
+/** 
+ * Enum like object which defines possible message types. Note that we can't
+ * use the Symbol type for Enum values, as they aren't cloneable. 
+ */
+const MessageType = Object.freeze({
+  ADD    : 1,
+  DELETE : 2
+});
+
+class ShortlinkMessenger {
+  /**
+   * @param {string} shortlink The shortlink to add. Should exclude the url
+   *                           scheme.
+   * @param {string} result The url string to map the shortlink to.
+   * @returns {Promise} Promise which is resolved with no message if the add was
+   *                    successful, or rejected with a reason if it wasn't. 
+   */
+  sendAddMessage(shortlink, result) {
+    return browser.runtime.sendMessage({
+      messageType: MessageType.ADD,
+      shortlink: shortlink,
+      result: result
+    });
+  }
+
+  /**
+   * @param {string} shortlink The shortlink to delete.
+   * @return {Promise} Promise which is resolved with no message id the delete
+   *                   was successful, or rejected with a reason if it wasn't.
+   */
+  sendDeleteMessage(shortlink) {
+    return browser.runtime.sendMessage({
+      messageType: MessageType.DELETE,
+      shortlink: shortlink
+    });
+  }
+}
+
+exports = {MessageType, ShortlinkMessenger};
